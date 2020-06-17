296e47413a92bac1a5e5744f9c83037ff6367cbe
Minwoo Yu <snowypainter@gmail.com>
2020-06-17 18:22:08 +0900

    Code new example

--- element_test.go
+++ element_test.go
@@ -5,26 +5,29 @@ import (
 	"testing"
 )
 
-func TestNewWindow(t *testing.T) {
-	window := NewWindow(NewSize(100, 200))
-	head := NewHead(NewSize(70, 200), "HeadyHeady")
-	body := NewBody(NewSize(30, 200), "BodyBody")
-	stack := NewLayout(StackLayoutType)
-	input := NewElement(InputElementType, NewRelativeLocation(5, 10))
+func myStackPlease() *LayoutElement {
+	stack := NewLayout(StackLayoutType, "MyLayout")
+	input := NewElement(InputElementType, "MyInput", NewRelativeLocation(5, 10))
 
 	err := stack.AddChild(input)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
-	err = body.AddChild(stack)
+
+	return stack
+}
+
+func TestNewWindow(t *testing.T) {
+	window := NewWindow(NewSize(32, 80))
+	body := NewBody(NewSize(32, 80), "MainBody")
+
+	stack := myStackPlease()
+
+	err := body.AddChild(stack)
 	if err != nil {
 		fmt.Println(err.Error())
 	}
 
-	err = window.Add(head)
-	if err != nil {
-		fmt.Println(err.Error())
-	}
 	err = window.Add(body)
 	if err != nil {
 		fmt.Println(err.Error())
