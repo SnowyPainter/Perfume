251c5b18100a7c8545521bdf6f327b44f0f43887
SnowyPainter <SnowyPainter@gmail.com>
2020-09-19 03:50:11 +0900

    Edit tests

--- perfume_test.go
+++ perfume_test.go
@@ -6,40 +6,64 @@ import (
 	"testing"
 )
 
-func TestRenderer(t *testing.T) {
-	testingSize := NewSize(5, 32)
+func createWindow() (*Window, error) {
+	testingSize := NewSize(20, 128)
+	h := NewSize(4, testingSize.Width)
+	b := NewSize(12, testingSize.Width)
+	f := NewSize(4, testingSize.Width)
 
 	window, err := NewWindow(testingSize)
 	if err != nil {
-		fmt.Println(err.Error())
-		return
+		return nil, err
 	}
 	//Manbody testing size applied
-	body := NewBody(testingSize, "MainBody")
+	head := NewHead(h, "Head")
+	body := NewBody(b, "Body")
+	foot := NewFooter(f, "Footer")
+	hlayout := NewLayout(StackLayoutType, h.Plus(-2), "HeadLayout")
 
 	borderOpt := CreateOption(reflect.TypeOf(""), nil, nil)
 	borderOpt.Set("*")
-	body.AddOption(
-		BorderOption,
-		borderOpt,
-	)
+	head.AddOption(BorderOption, borderOpt.Clone())
 
-	err = window.Add(body)
+	borderOpt.Set("=")
+	body.AddOption(BorderOption, borderOpt.Clone())
+
+	borderOpt.Set("-")
+	foot.AddOption(BorderOption, borderOpt.Clone())
+
+	borderOpt.Set("0")
+	hlayout.AddOption(BorderOption, borderOpt.Clone())
+
+	c, err := callSequence(
+		head.AddChild(hlayout),
+		window.Add(body),
+		window.Add(head),
+		window.Add(foot),
+	)
 	if err != nil {
-		fmt.Println(err, " : ", err.Error())
+		fmt.Println(c, " : ", err.Error())
+		return nil, err
+	}
+	return window, nil
+}
+
+func TestRenderer(t *testing.T) {
+	window, err := createWindow()
+	if err != nil {
+		fmt.Println(err)
 		return
 	}
-
 	renderer := NewRenderer(window)
+	printRendererStruct(renderer)
+	renderer.Render()
+}
 
-	renderer.PrintStruct(ElementsPrintDepth, map[PrintLineForm]*Parseable{
+func printRendererStruct(r *Renderer) {
+	r.PrintStruct(ElementsPrintDepth, map[PrintLineForm]*Parseable{
 		WindowLine:   NewParseable("Window || (", SizeProperty, ") || (", ChildrenLenProperty, ") ||\n\n"),
 		FormalsLine:  NewParseable("-- (", NameProperty, ") Formal --\n"),
 		LayoutsLine:  NewParseable("\t└--(", TypeProperty, ")layout ", NameProperty, "\n"),
 		ElementsLine: NewParseable("\t\t└--(", TypeProperty, ")element LOC:", RelLocationProperty, "\n"),
 	})
-
-	fmt.Println("rendering...")
-
-	renderer.Render()
 }
