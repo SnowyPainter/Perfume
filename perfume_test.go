9642d1adbb84232cc7f06241eb1c6e5a894a1457
SnowyPainter <SnowyPainter@gmail.com>
2020-09-22 23:04:41 +0900

    Revise tests

--- perfume_test.go
+++ perfume_test.go
@@ -3,45 +3,57 @@ package perfume
 import (
 	"fmt"
 	"testing"
+
+	"github.com/nathan-fiscaletti/consolesize-go"
 )
 
-func createWindow() (*Window, error) {
-	testingSize := NewSize(20, 128)
-	h := NewSize(4, testingSize.Width)
-	b := NewSize(12, testingSize.Width)
-	f := NewSize(4, testingSize.Width)
+//percent return percent that max 100
+func percent(v uint, p float32) float32 {
+	return float32(v) * (p / 100)
+}
+
+func createWindow(row, col uint) (*Window, error) {
+
+	//If use FullSize value, then you must get this 'real' value after NewWindow and check Error
+	fullSize := NewSize(FullSize, FullSize)
+	testingSize := fullSize //NewSize(row, col)
 
 	window, err := NewWindow(testingSize)
 	if err != nil {
 		return nil, err
 	}
+	winSize := window.size
+	h := NewSize(uint(percent(winSize.Height, 15)), winSize.Width)
+	b := NewSize(uint(percent(winSize.Height, 60)), winSize.Width)
+	f := NewSize(uint(percent(winSize.Height, 25)), winSize.Width)
+
 	//Manbody testing size applied
 	head := NewHead(h, "Head")
 	body := NewBody(b, "Body")
 	foot := NewFooter(f, "Footer")
-	bodyLayout := NewStackLayout("BodyLayout", b.Plus(-2), HorizontalOrientation, 1)
+	bodyLayout := NewStackLayout("BodyLayout", b.Plus(-2), VerticalOrientation, 1)
 	footLayout := NewFreeLayout("FootLayout", b.Plus(-2))
 
 	t1 := NewText("MyText1", "hello,", NewSize(1, 6))
 	t2 := NewText("MyText2", "world!", NewSize(1, 6))
 	t3 := NewText("MyText3", "It is foot layout", NewSize(1, foot.Size().Width-2))
 
-	t3Loc := NewRelativeLocation(int(foot.Size().Width/2), 0)
+	t3Loc := NewRelativeLocation(int(foot.Size().Width/2), 1)
 	t3.SetLocation(t3Loc)
 
 	borderOpt := NewOption(BorderOption, "")
-	fitOpt := NewOption(FitParentOption, false)
+	fitOpt := NewOption(FitParentOption, true)
 
-	borderOpt.Set("hd-")
+	borderOpt.Set("*")
 	head.AddOption(borderOpt.Clone())
 
-	borderOpt.Set("body-")
+	borderOpt.Set("*")
 	body.AddOption(borderOpt.Clone())
 
-	borderOpt.Set("ft-")
+	borderOpt.Set("*")
 	foot.AddOption(borderOpt.Clone())
 
-	borderOpt.Set("stcklayut-")
+	borderOpt.Set("=")
 	bodyLayout.AddOption(borderOpt.Clone())
 	bodyLayout.AddOption(fitOpt)
 
@@ -63,7 +75,9 @@ func createWindow() (*Window, error) {
 }
 
 func TestRenderer(t *testing.T) {
-	window, err := createWindow()
+
+	cols, rows := consolesize.GetConsoleSize()
+	window, err := createWindow(uint(rows), uint(cols))
 	if err != nil {
 		fmt.Println(err)
 		return
